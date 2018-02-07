89b3eeb2dd6f24e535f3962e1020c6724f05a49a
Geoff Kizer <geoffrek>
2018-02-06 19:15:03 -0800

    dns lookup test

---
+++ dnstest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Linq;
+
+namespace dnstest
+{
+    class Program
+    {
+        static void DumpIPHostEntry(IPHostEntry entry)
+        {
+            Console.WriteLine($"IPHostEntry for {entry.HostName}");
+            
+            foreach (var a in entry.AddressList)
+            {
+                Console.WriteLine($"Address: {a}");
+            }
+
+            foreach (var a in entry.Aliases)
+            {
+                Console.WriteLine($"Alias: {a}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var last = Dns.GetHostEntry("microsoft.com");
+            DumpIPHostEntry(last);
+
+            while (true)
+            {
+                Thread.Sleep(500);
+
+                var current = Dns.GetHostEntry("microsoft.com");
+
+                if (!Enumerable.SequenceEqual(current.AddressList, last.AddressList))
+                {
+                    Console.WriteLine($"{DateTime.UtcNow}: IPHostEntry.AddressList changed");
+                    DumpIPHostEntry(current);
+
+                    last = current;
+                }
+            }
+        }
+    }
+}

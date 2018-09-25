a8042a225a0ef7c94743798ff9647a0cc08e1eb7
Geoff Kizer <geoffrek>
2018-09-25 13:40:02 -0700

    add pool lock test code

---
+++ poollock/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading;
+
+namespace poollock
+{
+    class Program
+    {
+        const int s_port = 5000;
+        const int s_clientCount = 1000;
+
+        static readonly HttpClient s_httpClient = new HttpClient();
+
+        static async void RunClient()
+        {
+            while (true)
+            {
+                try
+                {
+                    var response = await s_httpClient.GetAsync("http://localhost:5000/");
+                    response.Dispose();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Client exception: {e}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var s = new Server();
+            s.Run(new IPEndPoint(IPAddress.Loopback, s_port));
+
+            for (int i = 0; i < s_clientCount; i++)
+            {
+                Thread.Sleep(10);
+                Task.Run(() => RunClient());
+            }
+
+            Console.WriteLine("All clients running");
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
---
+++ poollock/Server.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace poollock
+{
+    class Server
+    {
+        public Server()
+        {
+        }
+
+        public async void Run(IPEndPoint endPoint)
+        {
+            TcpListener listener = new TcpListener(endPoint);
+            listener.Start(1000);
+
+            while (true)
+            {
+                var conn = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("Connection accepted");
+
+                Task _ = Task.Run(() => HandleConnection(conn));
+            }
+        }
+
+        static readonly ReadOnlyMemory<byte> s_requestEnd = new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes("\r\n\r\n"));
+        static readonly ReadOnlyMemory<byte> s_response = new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\nContent-Length: 11\r\n\r\nHello World"));
+
+        public async void HandleConnection(TcpClient conn)
+        {
+            try
+            {
+                using (var stream = conn.GetStream())
+                {
+                    Memory<byte> buffer = new Memory<byte>(new byte[4096]);
+                    while (true)
+                    {
+                        int offset = 0;
+                        // Read request
+                        while (true)
+                        {
+                            int bytesRead = await stream.ReadAsync(buffer.Slice(offset));
+                            if (bytesRead == 0)
+                            {
+                                Console.WriteLine($"Connection closed");
+                                return;
+                            }
+
+//                            Console.WriteLine($"Read {bytesRead} bytes");
+
+                            offset += bytesRead;
+                            if (offset >= s_requestEnd.Length && 
+                                buffer.Slice(offset - s_requestEnd.Length, s_requestEnd.Length).Span.SequenceEqual(s_requestEnd.Span))
+                            {
+                                break;
+                            }
+
+                            if (offset == buffer.Length)
+                            {
+                                throw new Exception("request too long");
+                            }
+                        }
+
+//                        Console.WriteLine($"Sending response");
+
+                        // Send response
+                        await stream.WriteAsync(s_response);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                // eat it
+            }
+
+        }
+    }
+}
